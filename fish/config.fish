273b929a01018e10eaf1ac7acb3ab633fff5e91b
Pao Sheng <paooap.oappao@gmail.com>
2025-01-15 21:53:47 +0800

    feat: update config for fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,21 @@ end
 starship init fish | source
 
 # Created by `pipx` on 2024-06-24 06:26:11
-set PATH $PATH /Users/workspace/.local/bin
+set PATH $PATH /Users/paosheng/.local/bin
 
 # See https://fishshell.com/docs/current/interactive.html#vi-mode-commands for more details
 set -g fish_key_bindings fish_vi_key_bindings
+
+# Add pyenv executable to PATH by running
+# the following interactively:
+
+set -Ux PYENV_ROOT $HOME/.pyenv
+set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+
+# Load pyenv automatically by appending
+# the following to ~/.config/fish/config.fish:
+
+pyenv init - fish | source
+
+
+# Restart your shell for the changes to take effect.
