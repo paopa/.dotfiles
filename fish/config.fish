cef87c10f7a3ea4598d2b996ce2a454ec06f70ff
Pao Sheng <paooap.oappao@gmail.com>
2025-01-17 13:02:52 +0800

    feat: update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Load starship(https://starship.rs/)
 starship init fish | source
 
 # Created by `pipx` on 2024-06-24 06:26:11
@@ -12,14 +13,9 @@ set -g fish_key_bindings fish_vi_key_bindings
 
 # Add pyenv executable to PATH by running
 # the following interactively:
-
 set -Ux PYENV_ROOT $HOME/.pyenv
 set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
 
 # Load pyenv automatically by appending
-# the following to ~/.config/fish/config.fish:
-
-pyenv init - fish | source
-
-
-# Restart your shell for the changes to take effect.
+# comment out this one because it's will cause vscode or cursor to use pyenv python instead of poetry shell on terminal
+# pyenv init - fish | source
