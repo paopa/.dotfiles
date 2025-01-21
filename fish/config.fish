14ccef6d13398dea9b5ad40bb2540b3173ec3a0c
Pao Sheng <paooap.oappao@gmail.com>
2025-01-21 22:33:10 +0800

    feat: update fish shell config for clipboard in vi mode

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,11 @@ set PATH $PATH /Users/paosheng/.local/bin
 
 # See https://fishshell.com/docs/current/interactive.html#vi-mode-commands for more details
 set -g fish_key_bindings fish_vi_key_bindings
+# Refer to https://www.reddit.com/r/fishshell/comments/1e29h9q/vi_mode_with_clipboard/
+# Share system clipboard with vi mode
+bind -M visual y fish_clipboard_copy
+bind p fish_clipboard_paste
+# bind -M normal yy fish_clipboard_copy
 
 # Add pyenv executable to PATH by running
 # the following interactively:
